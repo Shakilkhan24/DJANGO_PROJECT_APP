dedbd00730ff171dfd7715e8c1e862bc46717cd5
SHAKIL <sk2448868@gmail.com>
2024-09-06 00:01:07 +0600

    removing urls of custom admin from the project urls as custom admin panel i am not going to use

--- project/project/urls.py
+++ project/project/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views  # Import Django's built-in 
 
 urlpatterns = [
     path('admin/', admin.site.urls),        # Default Django admin interface
-    path('custom-admin/', include('custom_admin.urls')),  # Custom admin application
+    # path('custom-admin/', include('custom_admin.urls')),  # Custom admin application
     path('', include('app.urls')),          # Your app's URLs
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),  # Ensure login view is accessible
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),  # Ensure logout view is accessible
